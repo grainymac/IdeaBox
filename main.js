d7f697ad3119b343d7ea3c80205c181c940454fc
Jenn Yacoubian <jyacoubian@gmail.com>
2022-07-28 16:18:34 -0600

    Added query sselectors

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+//Global Variables
 var ideaList = [];
+
+
+//Query Selectors
+var saveBtn = document.querySelector("#save-btn");
+var inputTitle = document.querySelector("#input-title");
+var inputBody = document.querySelector("#input-body");
+var cardTitle = document.querySelector("#card-title");
+var cardText = document.querySelector("#card-text");
+
+//Event Listerners
+saveBtn.eventListener("click", saveIdeaCard);
+
+//Functions
+function saveIdeaCard() {
+  var idea = new Idea(inputTitle.value, inputBody.value);
+}
