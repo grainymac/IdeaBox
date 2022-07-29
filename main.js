ec437b4c2a2894374d714b337c2f84292506c57d
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2022-07-29 17:21:15 -0500

    Delete idea boxes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Global Variables
 var ideaList = [];
 
-//Query Selectors
+//--------------------------------------------Query Selectors
 var saveBtn = document.querySelector("#save-btn");
 var inputTitle = document.querySelector("#input-title");
 var inputBody = document.querySelector("#input-body");
@@ -9,37 +9,41 @@ var cardTitle = document.querySelector("#card-title");
 var cardText = document.querySelector("#card-text");
 var singleCard = document.querySelector("#saved-ideas");
 var formSection = document.querySelector("#form-section");
-//Event Listerners
+var deleteCard = document.querySelector("#delete-card");
+
+//--------------------------------------------Event Listerners
 saveBtn.addEventListener("click", saveIdeaCard);
 formSection.addEventListener("input", disableButton);
-//Functions
+singleCard.addEventListener("click", deletingCard);
 
+//----------------------------------------------Functions
 function saveIdeaCard() {
   event.preventDefault();
   var idea = new Idea(inputTitle.value, inputBody.value);
   ideaList.push(idea);
-  singleCard.innerHTML += `<section class="single-card" id="single-card">
-    <div class="card-top-bar">
-      <img
-      class="star-img"
-      src="assets/star.svg"
-      alt="star icon"
-      />
-      <img
-      class="delete-img"
-      src="assets/delete.svg"
-      alt="delete icon"
-      />
-    </div>
-    <div class="card-body">
-      <h1 class="card-title" id="card-title">${inputTitle.value}</h1>
-      <p class="card-text" id="card-text">${inputBody.value}</p>
-    </div>
-    <div class="card-bottom-bar">
-      <img class="comment-img" src="assets/comment.svg" alt="comment icon"/>
-      <p class="comment">Comment</p>
-    </div>
-  </section>`;
+  reload();
+  // singleCard.innerHTML += `<section class="single-card" id="single-card">
+  //   <div class="card-top-bar">
+  //     <img
+  //     class="star-img"
+  //     src="assets/star.svg"
+  //     alt="star icon"
+  //     />
+  //     <img
+  //     class="delete-img"
+  //     src="assets/delete.svg"
+  //     alt="delete icon"
+  //     />
+  //   </div>
+  //   <div class="card-body">
+  //     <h1 class="card-title" id="card-title">${inputTitle.value}</h1>
+  //     <p class="card-text" id="card-text">${inputBody.value}</p>
+  //   </div>
+  //   <div class="card-bottom-bar">
+  //     <img class="comment-img" src="assets/comment.svg" alt="comment icon"/>
+  //     <p class="comment">Comment</p>
+  //   </div>
+  // </section>`;
   inputTitle.value = "";
   inputBody.value = "";
   if (inputTitle.value && inputBody.value) {
@@ -55,4 +59,48 @@ function disableButton() {
   } else {
     saveBtn.disabled = true;
   }
+}
+
+function deletingCard(event) {
+  var id = parseInt(event.target.id);
+  
+  for (var i = 0; i <ideaList.length; i++) {
+    console.log('hiii', ideaList[i].id);
+    if (ideaList[i].id === id) {
+      ideaList[i].id.splice(i, 1);
+      // i--;
+    }
+    console.log('hello', ideaList);
+    reload();
+  }
+}
+
+  function reload() {
+    singleCard.innerHTML = '';
+    for (var i = 0; i < ideaList.length; i++) {
+    singleCard.innerHTML += `<section class="single-card" id="single-card">
+    <div class="card-top-bar">
+      <img
+      class="star-img"
+      src="assets/star.svg"
+      alt="star icon"
+      />
+      <img
+      class="delete-img"
+      src="assets/delete.svg"
+      alt="delete icon"
+      />
+    </div>
+    <div class="card-body">
+      <h1 class="card-title" id="card-title">${ideaList[i].title}</h1>
+      <p class="card-text" id="card-text">${ideaList[i].body}</p>
+    </div>
+    <div class="card-bottom-bar">
+      <img class="comment-img" src="assets/comment.svg" alt="comment icon"/>
+      <p class="comment">Comment</p>
+    </div>
+  </section>`
+     
+  }
+
 }
