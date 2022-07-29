bcfdb0f6167e5d5f1cb23877828c75fc86ede980
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-07-28 19:04:19 -0600

    added new idea cards

--- main.js
+++ main.js
@@ -12,30 +12,38 @@ var singleCard = document.querySelector("#saved-ideas");
 saveBtn.addEventListener("click", saveIdeaCard);
 
 //Functions
+function disableButton() {
+  if (inputTitle.length > 0) {
+    saveBtn.addClassList.remove("disabled");
+  }
+}
+
 function saveIdeaCard() {
   event.preventDefault();
   var idea = new Idea(inputTitle.value, inputBody.value);
   ideaList.push(idea);
   singleCard.innerHTML += `<section class="single-card" id="single-card">
-  <div class="card-top-bar">
-    <img
+    <div class="card-top-bar">
+      <img
       class="star-img"
       src="assets/star.svg"
       alt="star icon"
-    />
-    <img
+      />
+      <img
       class="delete-img"
       src="assets/delete.svg"
       alt="delete icon"
-    />
-  </div>
-  <div class="card-body">
-    <h1 class="card-title" id="card-title">${inputTitle.value}</h1>
-    <p class="card-text" id="card-text">${inputBody.value}</p>
-  </div>
-  <div class="card-bottom-bar">
-    <img class="comment-img" src="assets/comment.svg" alt="comment icon"/>
-    <p class="comment">Comment</p>
-  </div>
+      />
+    </div>
+    <div class="card-body">
+      <h1 class="card-title" id="card-title">${inputTitle.value}</h1>
+      <p class="card-text" id="card-text">${inputBody.value}</p>
+    </div>
+    <div class="card-bottom-bar">
+      <img class="comment-img" src="assets/comment.svg" alt="comment icon"/>
+      <p class="comment">Comment</p>
+    </div>
   </section>`;
+  inputTitle.value = "";
+  inputBody.value = "";
 }
